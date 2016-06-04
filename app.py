cb3fc65b15a52812ae801dc286db902d96d9b4a7
Hao Dinh <h.c.dinh1@gmail.com>
2016-06-04 07:50:39 +0000

    p12

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import os
 from flask import Flask
+from flask import Flask, render_template
+
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/crisis')
+@app.route('/timeline')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('fd_timeline.html')
     
 @app.route('/user/<name>')
 def user(name):
-    return '<h1> Hello, %s! </h1>' % name
+    return render_template('user.html', name=name)
     
 app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
