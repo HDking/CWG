00d4d1addc2f424a98773d6cd330e44d9149be4a
Hao Dinh <h.c.dinh1@gmail.com>
2016-06-04 08:11:56 +0000

    eerste views

--- app.py
+++ app.py
@@ -8,10 +8,7 @@ app = Flask(__name__)
 
 @app.route('/timeline')
 def index():
-    return render_template('fd_timeline.html')
+    return render_template('/timeline.html')
     
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
     
 app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
